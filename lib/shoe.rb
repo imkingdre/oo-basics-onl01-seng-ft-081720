7030afd5ab2c0ff88a99326276e21cf277a09351
Andre King <andreking1031@gmail.com>
2020-08-26 14:35:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,6 +5,24 @@ class Shoe
   def brand
     @brand
   end
+  def color=(color)
+    @color = color
+  end
+  def color
+    @color
+  end
+  def size=(size)
+    @size = size
+  end
+  def size
+    @size
+  end
+  def material=(material)
+    @material = material
+  end
+  def material
+    @material
+  end
   
   
 end
